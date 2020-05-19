949f1576ee4e2774174ba673d0d81c20fc081cf2
Kirill Zaborsky <avto.oil@gmail.com>
2020-05-19 19:31:42 +0700

    Initial Commit

---
+++ HomeWork2.1-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork2.1-1
+//
+//  Created by Кирилл Заборский on 19.05.2020.
+//  Copyright © 2020 Кирилл Заборский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
